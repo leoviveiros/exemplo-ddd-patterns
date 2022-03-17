2bd0c84550547bf670541f057b6685e105066fa7
Leonardo Pinto <leoviveiros@gmail.com>
2022-03-17 10:12:14 -0300

    refatoracao regras negocio

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,36 +1,58 @@
+/*
+Complexidade de Negócio:
+- domain (regras de negocio)
+    - entity
+        - customer.ts 
+
+Complexidade Acidental
+- infra (mundo externo)
+    - model
+        - customer.ts (getters / setters)
+*/
 export class Customer {
     _id: string;
     _name: string;
-    _address: string;
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+
+        this.validade();
     }
 
-    get id(): string {
-        return this._id;
-    }
-
-    set id(id: string) {
-        this._id = id;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
+
+        this.validade();
     }
 
-    get address(): string {
-        return this._address;
-    }
-
-    set address(address: string) {
+    changeAddress(address: string) {
         this._address = address;
+
+        this.validade();
     }
 
+    activate() {
+        if (!this._address || this._address.length === 0) {
+            throw new Error("Customer must have an address");
+        }
+
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
+    }
+
+    validade() {
+        if (!this._id || this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+
+        if (!this._name || this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+    }
 }
