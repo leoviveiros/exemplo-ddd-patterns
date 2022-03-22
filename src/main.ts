5695e332b77de0f613de896c307aa91f2ba8d132
Leonardo Pinto <leoviveiros@gmail.com>
2022-03-22 10:37:07 -0300

    ajuste exec jest

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import OrderItem from './entity/order-item';
 
 // Customer Aggregate
 const customer = new Customer('123', 'John');
-const address = new Address('Rua Vergueiro', 300, '', '01310-200', 'São Paulo');
+const address = new Address('Rua Vergueiro', 300, '01310-200', 'São Paulo');
 
 customer.Address = address;
 customer.activate();
 
 // Order Aggregate
-const item1 = new OrderItem('1', 'Product 1', 10);
-const item2 = new OrderItem('2', 'Product 2', 20);
+const item1 = new OrderItem('1', 'Product 1', 10, '2', 2);
+const item2 = new OrderItem('2', 'Product 2', 20, '3', 1);
 
 const order = new Order('1', '123', [item1, item2]);
