e93a657639893bb847940083f2346553103a5ba6
Leonardo Pinto <leoviveiros@gmail.com>
2022-03-28 12:02:53 -0300

    refatoracao

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -39,43 +39,52 @@ describe('Order Repository Tests', () => {
         await sequelize.close();
     });
 
-    it('should create an order', async () => {
-        const customerRepository = new CustomerRepository();
-        const customer = new Customer('1', 'Customer 1')
-        const address = new Address('Street 1', 1, 'Zip Code 1', 'City 1');
-
-        customer.changeAddress(address);
-
-        await customerRepository.create(customer);
-
-        const productRepository = new ProductRepository();
-        const product = new Product('1', 'Product 1', 100);
-
-        await productRepository.create(product);
-
-        const orderItem = new OrderItem('1', product.name, product.price, product.id, 2);
-        const order = new Order('123', customer.id, [orderItem]);
+    describe('persistence tests', () => {
 
         const orderRepository = new OrderRepository();
-        await orderRepository.create(order);
+        let orderItem: OrderItem;
+        let order: Order;
 
-        const orderModel = await OrderModel.findOne({ where: { id: order.id }, include: ['items'] });
+        beforeEach(async () => {
+            const customerRepository = new CustomerRepository();
+            const customer = new Customer('1', 'Customer 1')
+            const address = new Address('Street 1', 1, 'Zip Code 1', 'City 1');
 
-        expect(orderModel.toJSON()).toStrictEqual({
-            id: order.id,
-            customer_id: customer.id,
-            total: order.total,
-            items: [{
-                id: orderItem.id,
-                name: orderItem.name,
-                price: orderItem.price,
-                product_id: orderItem.productId,
-                quantity: orderItem.quantity,
-                order_id: order.id
-            }],
+            customer.changeAddress(address);
+
+            await customerRepository.create(customer);
+
+            const productRepository = new ProductRepository();
+            const product = new Product('1', 'Product 1', 100);
+
+            await productRepository.create(product);
+
+            orderItem = new OrderItem('1', product.name, product.price, product.id, 2);
+            order = new Order('123', customer.id, [orderItem]);
         });
 
+        it('should create an order', async () => {
+            await orderRepository.create(order);
+
+            const orderModel = await OrderModel.findOne({ where: { id: order.id }, include: ['items'] });
+
+            expect(orderModel.toJSON()).toStrictEqual({
+                id: order.id,
+                customer_id: order.customerId,
+                total: order.total,
+                items: [{
+                    id: orderItem.id,
+                    name: orderItem.name,
+                    price: orderItem.price,
+                    product_id: orderItem.productId,
+                    quantity: orderItem.quantity,
+                    order_id: order.id
+                }],
+            });
+
+        });
     });
+    
 });
 
 // npm run tsc && jest src/infrastructure/repository/order.repository.spec.ts
